6068e46095fd49e65b006e46ba80070fb1fec465
ThomasRichardISEP <tho-richard@sfr.fr>
2017-05-26 12:33:47 +0200

    ajout weather en cours

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 // je suis Alexandre
 // je suis Anis le bg du gang le bg
-
+const httpClient = require('node-rest-client-promise').Client()
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
@@ -32,6 +32,21 @@ client.on('message', msg => {
         console.log(err)
       }
     })
+  }
+
+  if (msg.content.match(/!weather.*/)) {
+    var ville = msg.content.substring(9)
+    var urlmeteo = 'http://api.openweathermap.org/data/2.5/weather?q=' + ville + '&APPID=3054b2798248ff002957afc8655a64db'
+    httpClient.getPromise(urlmeteo)
+    .then(res => {
+      msg.channel.sendMessage(res.weather.description)
+      // console.log(res.response.statusCode)
+    })
+    .catch(err => {
+      console.log(err)
+      throw err
+    })
+    // console.log(urlmeteo.weather.description)
   }
 })
 
