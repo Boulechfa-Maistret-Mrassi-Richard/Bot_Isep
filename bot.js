17aef4d69322c79f0e436edcdc41a4678658b22e
ThomasRichardISEP <tho-richard@sfr.fr>
2017-06-11 18:25:15 +0200

    .

--- bot.js
+++ bot.js
@@ -111,6 +111,7 @@ client.on('message', msg => {
         // console.log(result)
         msg.channel.sendMessage("Bonjour! Je m'appelle : " + JSON.stringify(result.forms[0].name, null, 2) + ', je suis le pokemon numéro ' + JSON.stringify(result.id, null, 2) + '! Je suis un pokemon de type ' + JSON.stringify(result.types[0].type.name, null, 2) + '! Je mesure ' + JSON.stringify(result.height, null, 2) + ' pieds et pèse ' + JSON.stringify(result.weight, null, 2) + ' pounds!')
         // client.channel.setName('test')
+        msg.channel.setName('test')
       } else {
         console.log(error)
       }
