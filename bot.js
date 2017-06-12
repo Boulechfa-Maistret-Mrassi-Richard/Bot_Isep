0cf6a6178630f047d54717e1c5320e54f68a1404
Animar11 <anis.boulechfar@hotmail.fr>
2017-06-12 13:11:30 +0200

    m-à-j

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const client = new Discord.Client()
 
 var Spotifywebapinode = require('spotify-web-api-node')
 var spotifyWebApiNode = new Spotifywebapinode({
+  // mes info perso
   clientId: '9eebb5a810f144499c2ba420035fe63d',
   clientSecret: '3c5b4032ffe04793ba970f400cf28dc5'
 })
@@ -29,16 +30,16 @@ client.on('message', msg => {
                 msg.channel.sendMessage('Résultats probant numéro 3 pour la recherche track' + data.body.tracks.items[2].external_urls.spotify)
               })
             } else if (msg.content.match(/!spotify artist.*/)) {
-              var artiste = msg.content.substring(16)
-              spotifyWebApiNode.searchArtists(artiste)
+              var chanteur = msg.content.substring(16)
+              spotifyWebApiNode.searchArtists(chanteur)
               .then(function (data) {
                 msg.channel.sendMessage('Résultats  probant numéro 1 pour la recherche artiste ' + data.body.artists.items[0].external_urls.spotify)
                 msg.channel.sendMessage('Résultats  probant numéro 2 pour la recherche artiste ' + data.body.artists.items[1].external_urls.spotify)
                 msg.channel.sendMessage('Résultats  probant numéro 3 pour la recherche artiste ' + data.body.artists.items[2].external_urls.spotify)
               })
             } else if (msg.content.match(/!spotify album.*/)) {
-              var album = msg.content.substring(15)
-              spotifyWebApiNode.searchAlbums(album)
+              var disque = msg.content.substring(15)
+              spotifyWebApiNode.searchAlbums(disque)
               .then(function (data) {
                 msg.channel.sendMessage('Résultats  probant numéro 1 pour la recherche album ' + data.body.albums.items[0].external_urls.spotify)
                 msg.channel.sendMessage('Résultats  probant numéro 2 pour la recherche album ' + data.body.albums.items[1].external_urls.spotify)
